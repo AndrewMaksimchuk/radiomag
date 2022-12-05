39a10ae1add7d2fa9da04fa26dd41b98d30aef03
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-12-05 20:44:40 +0200

    Fixed: Props in typescript style.

--- radiomag-client-vue/src/components/HeaderNavItem.vue
+++ radiomag-client-vue/src/components/HeaderNavItem.vue
@@ -1,18 +1,12 @@
 <template>
-    <li class="header-nav__item">
-        <RouterLink :to="link" class="link">{{ text }}</RouterLink>
-    </li>
+  <li class="header-nav__item">
+    <RouterLink :to="link" class="link">{{ text }}</RouterLink>
+  </li>
 </template>
 
 <script setup lang="ts">
-defineProps({
-    link: {
-        type: String,
-        default: '/',
-    },
-    text: {
-        type: String,
-        default: '',
-    },
-});
+defineProps<{
+  link: string;
+  text: string;
+}>();
 </script>
