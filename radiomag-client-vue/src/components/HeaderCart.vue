c227919a0e59441f7a5fe407210274e574a72487
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-12-06 23:37:26 +0200

    Changed: Replace select lang from html to component + eslint + stylelint.

--- radiomag-client-vue/src/components/HeaderCart.vue
+++ radiomag-client-vue/src/components/HeaderCart.vue
@@ -1,5 +1,5 @@
 <template>
-  <RouterLink :to="{ name: 'cart' }">
+  <RouterLink :to="{ name: 'cart' }" class="cart" tabindex="-1">
     <button class="header__info-item card">
       <img
         src="@/assets/images/shopping-cart.svg"
@@ -13,7 +13,14 @@
 
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
-import { useCart } from '@/store/cart';
+import { useCart } from "@/store/cart";
 
 const cart = useCart();
 </script>
+
+<style lang="scss">
+.cart {
+  display: block;
+  height: 100%;
+}
+</style>
--- radiomag-client-vue/src/components/HeaderSearchField.vue
+++ radiomag-client-vue/src/components/HeaderSearchField.vue
@@ -1,21 +1,35 @@
 <template>
-    <form action="/" class="search__form">
-        <input type="text" :placeholder="searchPlaceholder" class="search__form-input" v-model="productName" />
-        <button type="submit" class="search__form-submit" @click.prevent="searchProduct">
-            <img src="@/assets/images/magnifying-glass.svg" alt="search button" class="search__form-button" />
-        </button>
-    </form>
+  <form action="/" class="search__form">
+    <input
+      type="text"
+      :placeholder="searchPlaceholder"
+      class="search__form-input"
+      v-model="productName"
+    />
+    <button
+      type="submit"
+      class="search__form-submit"
+      @click.prevent="searchProduct"
+    >
+      <img
+        src="@/assets/images/magnifying-glass.svg"
+        alt="search button"
+        class="search__form-button hover"
+      />
+    </button>
+  </form>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const router = useRouter();
-const productName = ref('');
-const searchPlaceholder = ref('Введіть текст');
+const productName = ref("");
+const searchPlaceholder = ref("Введіть текст");
 
-const searchProduct = () => productName.value 
-    ? router.push({name: 'search', params: { name: productName.value }})
-    : searchPlaceholder.value = 'Поле пусте, потрібно ввести текст';
+const searchProduct = () =>
+  productName.value
+    ? router.push({ name: "search", params: { name: productName.value } })
+    : (searchPlaceholder.value = "Поле пусте, потрібно ввести текст");
 </script>
