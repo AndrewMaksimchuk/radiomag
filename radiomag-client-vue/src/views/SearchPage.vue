e2233e2f1e45cd47eea7ffe9efc2b115276b4c1b
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-02-28 16:16:34 +0200

    Added: Translate and min height (#43).

--- radiomag-client-vue/src/views/SearchPage.vue
+++ radiomag-client-vue/src/views/SearchPage.vue
@@ -1,19 +1,19 @@
-// https://www.rcscomponents.kiev.ua/modules.php?name=Asers_Shop&s_op=search&query=mega328
+//
+https://www.rcscomponents.kiev.ua/modules.php?name=Asers_Shop&s_op=search&query=mega328
 <template>
   <section class="container search-box">
-    <h2 class="search-box__header-text">Результат пошуку для "{{ $route.params.name }}":</h2>
+    <h2 class="search-box__header-text">
+      {{ $t("search") }} "{{ $route.params.name }}":
+    </h2>
     <!-- <CardLine/> -->
     <!-- <CardLine/> -->
     <!-- <CardLine/> -->
   </section>
 </template>
 
-<script setup lang="ts">
-import CardLine from '@/components/CardLine.vue';
-</script>
-
 <style lang="scss">
 .search-box {
+  min-height: 100vh;
   padding-bottom: 23px;
   display: flex;
   flex-direction: column;
