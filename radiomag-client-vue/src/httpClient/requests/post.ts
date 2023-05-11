6343214f6bf53f0851969d0b7bae6b8f70d02a8b
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-05-11 23:10:55 +0300

    Added: Ping service.

--- radiomag-client-vue/src/httpClient/requests/post.ts
+++ radiomag-client-vue/src/httpClient/requests/post.ts
@@ -1,12 +1,20 @@
 import type { FormData, OrderResponse } from "../../../../dto/Order";
+import type { PingPayload } from "../../../../dto/Ping";
 import { HTTPpost } from "../../../../endpoints";
 import POSTRequest from "../methods/POST";
+
+export interface DefaultResponse {
+  ok: boolean;
+}
 
 export const POST = {
   order: async (data: FormData) => {
     return await POSTRequest<OrderResponse>(HTTPpost.order, data);
   },
   error: async (data: Record<string, unknown>) => {
-    return await POSTRequest<void>(HTTPpost.error, data);
+    return await POSTRequest<DefaultResponse>(HTTPpost.error, data);
+  },
+  ping: (data: PingPayload) => {
+    POSTRequest(HTTPpost.ping, data);
   },
 };
---
+++ radiomag-client-vue/src/services/PingService.ts
@@ -0,0 +1,23 @@
+import type { PingPayload } from "../../../dto/Ping";
+import { POST } from "@/httpClient";
+import { useUser } from "@/models/user";
+
+type Remove<T> = T extends PingPayload
+  ? Omit<T, "time" | "user" | "from">
+  : never;
+
+type PingData = Remove<PingPayload>;
+
+export class PingService {
+  public static async ping(data: PingData) {
+    const time = Date.now();
+    const user = useUser().id;
+
+    if (data.action === "go to page") {
+      const from = document.location.pathname;
+      return POST.ping({ ...data, time, user, from });
+    }
+
+    POST.ping({ ...data, time, user });
+  }
+}
