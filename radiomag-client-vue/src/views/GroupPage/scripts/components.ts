01fae0e906e08cede01615511a4a295026451250
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-08-30 20:13:35 +0300

    Added: Switch view in group, show all goods(#83).

---
+++ radiomag-client-vue/src/views/GroupPage/scripts/components.ts
@@ -0,0 +1,19 @@
+import { SkeletonGroup } from "@/components/SkeletonGroupComponent";
+import { GroupSearch } from "@/components/GroupSearchComponent";
+import { Filters } from "@/components/FiltersComponent";
+import { CardTable } from "@/components/CardTableComponent";
+import { ToggleGroupView } from "@/components/ToggleGroupViewComponent";
+import Pagination from "@/components/PaginationComponent.vue";
+import CardLine from "@/components/CardLineComponent.vue";
+import ErrorMessageInGroup from "@/components/ErrorMessageInGroup.vue";
+
+export const components = {
+  SkeletonGroup,
+  Pagination,
+  CardLine,
+  ErrorMessageInGroup,
+  Filters,
+  GroupSearch,
+  CardTable,
+  ToggleGroupView,
+};
