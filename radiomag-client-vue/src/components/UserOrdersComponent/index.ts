360876a6c31d12d359009808c0900a664b82fe1c
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-09-12 14:52:01 +0300

    Added: New empty component for UserPage.

---
+++ radiomag-client-vue/src/components/UserOrdersComponent/index.ts
@@ -0,0 +1,1 @@
+export { default as UserOrders } from "./UserOrdersComponent.vue";
---
+++ radiomag-client-vue/src/components/index.ts
@@ -0,0 +1,5 @@
+export { UserHistory } from "./UserHistoryComponent";
+export { UserNotification } from "./UserNotificationComponent";
+export { UserOrders } from "./UserOrdersComponent";
+export { UserSettings } from "./UserSettingsComponent";
+export { UserTracking } from "./UserTrackingComponent";
