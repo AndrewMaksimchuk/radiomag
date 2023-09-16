0d9265faa73b3c7e4a2319d58a91f53abcde84ab
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-09-16 13:29:14 +0300

    Added: Mocks - router, userStore. Tests.

--- radiomag-client-vue/src/views/UserPage/script.ts
+++ radiomag-client-vue/src/views/UserPage/script.ts
@@ -13,7 +13,7 @@ export default defineComponent({
     const router = useRouter();
     const activeTab = ref("orders");
 
-    const handleClick = (tab: TabsPaneContext) => {
+    const handleTabClick = (tab: TabsPaneContext) => {
       const { index } = tab;
       if (undefined === index) {
         return;
@@ -38,7 +38,7 @@ export default defineComponent({
       onLogout,
       tabs,
       activeTab,
-      handleClick,
+      handleTabClick,
     };
   },
 });
